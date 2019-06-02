2879133e2cbecb363f738c1c87db982660dd7e0f
Yuki <yukinsnow@gmail.com>
2019-06-02 20:36:37 +1000

    add prototype and ppt

---
+++ Prototype/js/config.js
@@ -0,0 +1,1 @@
+window.config={"logo":"","background":"#C4C4C4","shell":"iPhone","showControlPanel":false,"iOSGuide":false,"alwaysShowLinks":false,"hoverShowLinks":false,"hoverShowRemarks":true,"showComments":true,"showQRCode":false,"hintLinks":true,"scrollRemarks":true,"isShowAllPageNode":false,"isShowPreviewTools":false,"showRemarkFlag":false}
